ccc5bc2af2a3ba0e814b013e31159f233dc68bcb
Douglas <wdmdavison@icloud.com>
2024-01-08 20:57:08 +0000

    1 for testing

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+var count = 0;
+
+function updateCounter(){
+    count += 1;
+    $('#count').text(`${count} clicks on the button`)
+}
+
+$(function() {
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button id="get_started">Click here to get started</button>');
+    $('body').append("<p id='count'>0 clicks on the button</p>")
+    $('body').append('<p>Copyright - Holberton School</p>');
+    $('#get_started').on('click', _.debounce(updateCounter, 500));
+});
