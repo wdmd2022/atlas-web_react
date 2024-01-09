e4c2cb5ba7439e3d7deeb7ab5156ff022243fece
Douglas <wdmdavison@icloud.com>
2024-01-09 16:14:38 -0600

    update 2

--- react_intro/task_2/dashboard/src/App.js
+++ react_intro/task_2/dashboard/src/App.js
@@ -15,6 +15,11 @@ function App() {
         <p>
           Login to access the full dashboard
         </p>
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" name="password" />
+        <button>OK</button>
       </div>
       <footer className="App-footer">
         <p>
--- react_intro/task_2/dashboard/src/utils.js
+++ react_intro/task_2/dashboard/src/utils.js
@@ -12,4 +12,8 @@ function getFooterCopy(isIndex) {
     }
 }
 
-export { getFullYear, getFooterCopy };
+function getLatestNotification() {
+    return "<strong>Urgent requirement</strong> - complete by EOD"
+}
+
+export { getFullYear, getFooterCopy, getLatestNotification };
