1553fc79fa684ca1b84eff678794d251335ef769
Douglas <wdmdavison@icloud.com>
2024-01-10 21:55:44 -0600

    very very good

---
+++ react_props/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        static: path.join(__dirname, '../dist'),
+        port: 8564,
+        hot: true,
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(?:ico|gif|png|jpeg|jpg)$/i,
+                use: [
+                    'file-loader',
+                    'image-webpack-loader'
+                ]
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+    entry: path.resolve(__dirname, '../src/index.js'),
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'inline-source-map',
+}
---
+++ react_props/task_5/dashboard/src/CourseList/CourseList.test.js
@@ -0,0 +1,18 @@
+import CourseList from "./CourseList";
+import CourseListRow from "./CourseListRow";
+import React from 'react';
+import { shallow } from 'enzyme';
+
+describe('<CourseList />', () => {
+  it('Totally does not crash when it renders', () => {
+    // we can prove it by making a shallow render and hoping for the best
+    shallow(<CourseList />);
+    // nice
+  });
+
+  it('Renders the 5 different rows we were hoping for', () => {
+    // first let's make a new wrapper (what is this, a wrapper convention?)
+    const wrapper = shallow(<CourseList />);
+    expect(wrapper.find(CourseListRow).length).toBe(5);
+  });
+});
