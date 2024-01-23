f42f1b5adec807117e5608cce651007e86af395f
Douglas <wdmdavison@icloud.com>
2024-01-23 15:49:38 -0600

    notifications panel fullscreen when small screen

--- React_inline_styling/task_3/dashboard/src/Notifications/Notifications.js
+++ React_inline_styling/task_3/dashboard/src/Notifications/Notifications.js
@@ -18,7 +18,24 @@ const styles = StyleSheet.create({
     right: '3%',
     width: '40%',
     height: '10%',
-    fontSize: 'calc(6px + 1vmin)'
+    fontSize: 'calc(6px + 1vmin)',
+    '@media (max-width: 900px)': {
+      // let's make it big when the screen is small
+      position: 'fixed',
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      margin: 0,
+      padding: 0,
+      border: 'none',
+      fontSize: '20px',
+      zIndex: 10,
+      overflow: 'auto',
+      background: 'white'
+    }
   },
   menuItem: {
     marginTop: '0px',
