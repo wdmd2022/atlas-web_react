fa05483bb5c194792b658d8bcaa1b1a42a7d1d82
Douglas <wdmdavison@icloud.com>
2024-01-24 11:08:31 -0600

    created state variables for login

---
+++ react_state/task_1/dashboard/src/App/App.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import { useState } from 'react';
+import Notifications from '../Notifications/Notifications';
+import Login from '../Login/Login';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import PropTypes from 'prop-types';
+import CourseList from '../CourseList/CourseList';
+import BodySection from '../BodySection/BodySection';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  body: {
+    textAlign: 'center',
+    backgroundColor: 'white',
+    fontFamily: 'serif',
+    paddingTop: '0px'
+  },
+  footer: {
+    fontStyle: 'italic',
+    fontSize: 'calc(10px + 1vmin)',
+    textAlign: 'center',
+    backgroundColor: 'white',
+    fontFamily: 'serif',
+    borderTop: '2px solid red',
+    marginTop: '200px'
+  }
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { displayDrawer: false };
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+  }
+  componentDidMount() {
+    document.addEventListener('keydown', this.pressyWessyCtrlH);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.pressyWessyCtrlH);
+  }
+
+  pressyWessyCtrlH = (event) => {
+    if (event.ctrlKey && event.key === 'h') {
+      event.preventDefault();
+      alert("logging you out");
+      this.props.logOut();
+    }
+  }
+
+  handleDisplayDrawer() {
+    this.setState({ displayDrawer: true });
+  }
+
+  handleHideDrawer() {
+    this.setState({ displayDrawer: false });
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+    const { logOut } = this.props;
+    const { displayDrawer } = this.state;
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } }
+    ];
+
+    return (
+      <>
+        <Notifications
+          listNotifications={listNotifications}
+          displayDrawer={displayDrawer}
+          handleDisplayDrawer={this.handleDisplayDrawer}
+          handleHideDrawer={this.handleHideDrawer}
+        />
+        <div className={css(styles.body)}>
+          <Header />
+          {isLoggedIn?
+          <BodySectionWithMarginBottom title="Course list" children={<CourseList listCourses={listCourses} />} />
+          :
+          <BodySectionWithMarginBottom title="Log in to continue" children={<Login />} />
+          }
+          <BodySection title="News from the School" children={<p>School is cool. School is cool.</p>} />
+          <Footer className={css(styles.footer)} />
+        </div>
+      </>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {},
+};
+
+export default App;
---
+++ react_state/task_1/dashboard/src/Login/Login.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import { useState } from 'react';
+
+const styles = StyleSheet.create({
+  body: {
+    background: 'white',
+    fontFamily: 'serif',
+    fontSize: 'calc(12px + 1vmin)',
+    textAlign: 'left',
+    padding: '50px',
+    paddingBottom: '50px'
+  },
+  formContainer: {
+    '@media (min-width: 900px)': {
+      display: 'flex',
+      alignItems: 'center'
+    }
+  },
+  formGroup: {
+    display: 'flex',
+    marginBottom: '20px',
+    alignItems: 'center',
+    marginRight: '50px',
+    '@media (max-width: 900px)' : {
+      alignItems: 'flex-start',
+      width: '100%',
+      marginBottom: '5px'
+      // flexDirection: 'column'
+    }
+  },
+  label: {
+    marginRight: '10px',
+    marginBottom: '5px',
+    '@media (max-width: 900px)': {
+      marginBottom: '0px'
+    }
+  },
+  input: {
+    marginRight: '20px',
+    flexGrow: 1,
+    '@media (max-width: 899px)': {
+      border: 'none',
+    }
+  },
+  button: {
+    margin: '2px',
+    '@media (max-width: 899px)': {
+      border: 'none',
+      backgroundColor: 'white',
+      boxShadow: '0 0 12px 3px #DAA520',
+      borderRadius: '2px',
+      paddingLeft: '10px',
+      paddingRight: '10px',
+      paddingTop: '5px',
+      paddingBottom: '5px',
+    },
+    '@media (min-width: 900px)': {
+      flexGrow: 0,
+      marginLeft: '10px',
+      marginBottom: '10px',
+    }
+  }
+});
+
+function Login() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <div className={css(styles.body)}>
+      <p>
+          Login to access the full dashboard
+      </p>
+      <div className={css(styles.formContainer)}>
+        <div className={css(styles.formGroup)}>
+          <label htmlFor="email" className={css(styles.label)}>Email: </label>
+          <input type="email" id="email" name="email" className={css(styles.input)} />
+        </div>
+        <div className={css(styles.formGroup)}>
+          <label htmlFor="password" className={css(styles.label)}>Password: </label>
+          <input type="password" id="password" name="password" className={css(styles.input)} />
+        </div>
+      <button className={css(styles.button)}>OK</button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
