0ecf21390fedc2d2948f3592fad670cfff26daa6
Douglas <wdmdavison@icloud.com>
2024-01-24 23:16:10 -0600

    removing payload

---
+++ 0x08_react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.js
@@ -0,0 +1,17 @@
+import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes';
+
+// The function selectCourse will accept index as argument
+export function selectCourse(index) {
+  return {
+    type: SELECT_COURSE,
+    payload: index,
+  };
+}
+
+// The function unSelectCourse will accept index as argument
+export function unSelectCourse(index) {
+  return {
+    type: UNSELECT_COURSE,
+    payload: index,
+  };
+}
