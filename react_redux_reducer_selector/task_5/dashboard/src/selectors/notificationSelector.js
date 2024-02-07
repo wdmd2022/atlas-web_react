e1918204eb5990914fb96ddaad1a562a730043a9
Douglas <wdmdavison@icloud.com>
2024-02-07 16:14:00 -0600

    removed reselect from task 5

--- react_redux_reducer_selector/task_5/dashboard/src/selectors/notificationSelector.js
+++ react_redux_reducer_selector/task_5/dashboard/src/selectors/notificationSelector.js
@@ -1,8 +1,14 @@
-import { createSelector } from 'reselect';
+// let's make some selectors that will accept the Redux store state
+// and return data based on that state. Given that the state is
 
-export const filterTypeSelected = state => state.get('filter');
-export const getNotifications = state => state.get('notifications');
-export const getUnreadNotifications = createSelector(
-  getNotifications,
-  (notifications) => notifications.filter(notif => !notif.get('isRead'))
-);
+// this one will return the value of the filter
+export const filterTypeSelected = (state) => state.get('filter');
+
+// this one will return the list of notifications
+export const getNotifications = (state) => state.get('notifications');
+
+// and this one, just the unread ones.
+export const getUnreadNotifications = (state) => {
+  const notifsToFilter = state.get('notifications');
+  return notifsToFilter.filterNot(notif => notif.get('isRead'));
+}
